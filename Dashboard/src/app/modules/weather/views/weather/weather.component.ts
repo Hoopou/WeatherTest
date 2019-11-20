10fd3a352ff07be6efa948eea06a1e095dea106f
Vincent_Boutot <VincentBoutot@live.ca>
2019-11-20 17:13:37 -0500

    ajout de la mediane

--- Dashboard/src/app/modules/weather/views/weather/weather.component.ts
+++ Dashboard/src/app/modules/weather/views/weather/weather.component.ts
@@ -67,6 +67,26 @@ export class WeatherComponent implements OnInit {
     return min;
   }
 
+  getMed(){
+    let values = [];
+
+    this.data.list.forEach(element => {
+      values.push(element.deg/10);
+    });
+
+    if(values.length ===0) return 0;
+  
+    values.sort(function(a,b){
+      return a-b;
+    });
+  
+    var half = Math.floor(values.length / 2);
+  
+    if (values.length % 2)
+      return values[half];
+  
+    return (values[half - 1] + values[half]) / 2.0;
+  }
 
 
 }
